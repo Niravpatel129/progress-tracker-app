a38999914257ffb3c964751f91a42e9dfb2e972e
Nirav Patel <niravpatelp129@gmail.com>
2023-09-05 05:08:54 -0400

    inital

--- App.js
+++ App.js
@@ -1,20 +1,162 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Camera } from 'expo-camera';
+import * as FileSystem from 'expo-file-system';
+import React, { useEffect, useRef, useState } from 'react';
+import {
+  FlatList,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native';
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [cameraRef, setCameraRef] = useState(null);
+  const [lastSelfieUri, setLastSelfieUri] = useState(null);
+  const [selfieUris, setSelfieUris] = useState([]);
+  const flatListRef = useRef(null);
+  const [selectedImageUri, setSelectedImageUri] = useState(null);
+  const [showTrashIcon, setShowTrashIcon] = useState(false);
+  const [lastVisibleIndex, setLastVisibleIndex] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const takePicture = async () => {
+    if (cameraRef) {
+      const photo = await cameraRef.takePictureAsync();
+      const newFileUri = FileSystem.documentDirectory + new Date().getTime() + '.jpg';
+      await FileSystem.moveAsync({
+        from: photo.uri,
+        to: newFileUri,
+      });
+      setLastSelfieUri(newFileUri);
+      setSelfieUris((prevSelfieUris) => [...prevSelfieUris, newFileUri]);
+      flatListRef.current.scrollToEnd({ animated: true });
+    }
+  };
+
+  const toggleCamera = () => {
+    setSelectedImageUri(null);
+    setShowTrashIcon(false);
+  };
+
+  const deleteSelectedImage = async () => {
+    if (selectedImageUri) {
+      // Delete the file from the filesystem
+      await FileSystem.deleteAsync(selectedImageUri);
+
+      // Remove the URI from selfieUris
+      const newSelfieUris = selfieUris.filter((uri) => uri !== selectedImageUri);
+      setSelfieUris(newSelfieUris);
+
+      // Update lastSelfieUri if needed
+      if (lastSelfieUri === selectedImageUri) {
+        setLastSelfieUri(newSelfieUris.length > 0 ? newSelfieUris[newSelfieUris.length - 1] : null);
+      }
+
+      // Update selectedImageUri to next available image or null
+      setSelectedImageUri(
+        newSelfieUris.length > 0 ? newSelfieUris[newSelfieUris.length - 1] : null,
+      );
+      setShowTrashIcon(newSelfieUris.length > 0);
+    }
+  };
+
+  const handleImageClick = (uri) => {
+    setSelectedImageUri(uri);
+    setShowTrashIcon(true);
+  };
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      <TouchableWithoutFeedback onPress={selectedImageUri ? null : takePicture}>
+        <Camera style={{ flex: 1 }} ref={(ref) => setCameraRef(ref)}>
+          {lastSelfieUri && !selectedImageUri && (
+            <Image source={{ uri: lastSelfieUri }} style={styles.fullScreenImage} />
+          )}
+          {selectedImageUri && (
+            <Image source={{ uri: selectedImageUri }} style={styles.fullScreenImagePreview} />
+          )}
+        </Camera>
+      </TouchableWithoutFeedback>
+      {showTrashIcon && (
+        <TouchableOpacity style={styles.trashIconContainer} onPress={deleteSelectedImage}>
+          <Text style={styles.trashIcon}>🗑️</Text>
+        </TouchableOpacity>
+      )}
+      <View style={styles.carouselContainer}>
+        <FlatList
+          ref={flatListRef}
+          data={[...selfieUris, { isCameraIcon: true }]}
+          horizontal={true}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({ item }) =>
+            !item.isCameraIcon ? (
+              <TouchableOpacity onPress={() => handleImageClick(item)}>
+                <Image source={{ uri: item }} style={styles.carouselImage} />
+              </TouchableOpacity>
+            ) : (
+              <TouchableOpacity onPress={toggleCamera}>
+                <Text style={styles.cameraIcon}>📷</Text>
+              </TouchableOpacity>
+            )
+          }
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  carouselContainer: {
+    height: 100,
+    backgroundColor: 'white',
+  },
+  carouselImage: {
+    width: 100,
+    height: 100,
+  },
+  cameraIcon: {
+    fontSize: 48,
+    margin: 25,
+  },
+  fullScreenImage: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    opacity: 0.5,
+  },
+  fullScreenImagePreview: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    opacity: 1,
+  },
+  trashIconContainer: {
+    position: 'absolute',
+    right: 20,
+    bottom: 160,
+    zIndex: 2,
+  },
+  trashIcon: {
+    fontSize: 36, // Made the icon smaller
   },
 });
